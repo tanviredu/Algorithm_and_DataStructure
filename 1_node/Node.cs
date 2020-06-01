1a9092beb93e502bb903b382e965cc8d5c58007e
tanviredu <ornobtanvir.git@gmail.com>
2020-06-01 16:58:48 +0600

    added

---
+++ 1_node/Node.cs
@@ -0,0 +1,11 @@
+namespace _1_node
+{
+    public class Node{
+        // this class contain a value for the node
+        // and a node placeholder for connecting 
+        // the next node
+        public int Value {get;set;}
+        public Node Next{get;set;}
+        
+    }
+}
---
+++ 1_node/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _1_node
+{
+    class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            
+            
+           // creating three independent node 
+            
+            Node first = new Node();
+            first.Value =3;
+            
+            
+            Node second = new Node();
+            second.Value = 5;
+            
+            Node third = new Node();
+            third.Value = 10;
+            
+            
+            // adding the send node
+            // to the first nodes Next placeholder
+            // to make a connection
+            first.Next = second;
+            second.Next = third;
+            
+            PrintChain(first); // this is the starting of the chain
+            
+            
+        }
+
+        private static void PrintChain(Node node)
+        {
+            while(node != null){
+                Console.Write($"Node {node.Value,2}");
+                node = node.Next;
+                Console.Write($" -----> ");   
+            }
+
+        }
+        
+        
+    }
+}
