f5a8e4bfa1b0ce0c8d3ade94df8c7ff4a82ec502
tanviredu <ornobtanvir.git@gmail.com>
2020-06-02 15:47:21 +0600

    added

---
+++ 3_Stack/default_stack/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace default_stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // init default stack
+            Stack<string> numbers = new Stack<string>();
+            numbers.Push("One");
+            numbers.Push("Two");
+            numbers.Push("Third");
+            numbers.Push("Four");
+            Console.WriteLine(numbers.Peek());
+            numbers.Pop();
+            numbers.Pop();
+            foreach(var i in numbers){
+                Console.WriteLine(i);
+            }
+        }
+    }
+    
+    
+}
